a70edfed33e74154861310bb948636f28671df82
User <User@LAB3-20>
2021-09-13 14:33:04 +0300

    Add project files.

---
+++ MyGanServer/Controllers/MyGanController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyGanServerBL.Models;
+
+namespace MyGanServer.Controllers
+{
+    [Route("MyGanAPI")]
+    [ApiController]
+    public class MyGanController : ControllerBase
+    {
+        #region Add connection to the db context using dependency injection
+        MyGanDBContext context;
+        public MyGanController(MyGanDBContext context)
+        {
+            this.context = context;
+        }
+        #endregion
+    }
+}
---
+++ MyGanServerBL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyGanServerBL
+{
+    public class Class1
+    {
+    }
+}
