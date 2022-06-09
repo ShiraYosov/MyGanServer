65001c0c9ea299cb78d6161fb0649e5f7c8f5dbd
User <User@LAB3-7>
2022-06-09 14:48:26 +0300

    email bug fix

--- MyGanServer/Helper/EmailSender.cs
+++ MyGanServer/Helper/EmailSender.cs
@@ -4,34 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Mail;
+using SendGridLib;
+
 
 namespace MyGanServer.Helper
 {
     public class EmailSender
     {
-        public static void SendEmail(string subject, string body, string to, string toName, string from, string fromName, string pswd, string smtpUrl)
+        public static async void SendEmail2(string subject, string body, string to, string toName, string from, string fromName, string pswd, string smtpUrl)
         {
-            var fromAddress = new MailAddress(from, fromName);
-            var toAddress = new MailAddress(to, toName);
-            string fromPassword = pswd;
-
-            var smtp = new SmtpClient
-            {
-                Host = smtpUrl,
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
-                Timeout = 20000
-            };
-            using (var message = new MailMessage(fromAddress, toAddress)
-            {
-                Subject = subject,
-                Body = body
-            })
-            {
-                smtp.Send(message);
-            }
+           await MailSender.SendEmail(fromName, to, toName, subject, body, "");
         }
     }
 }
